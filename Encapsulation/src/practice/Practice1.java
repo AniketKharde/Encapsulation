c5996e25b7dc2ea8d7eba1d0d96fd0d468482947
Aniket <aniketkharde17@gmail.com>
2023-04-17 21:49:51 +0530

    new jira -141

---
+++ Encapsulation/src/practice/Practice1.java
@@ -0,0 +1,12 @@
+package practice;
+
+import encapsulation2.ChildClass2;
+
+public class Practice1 {
+
+	
+	public static void main(String[] args) {
+		ChildClass2 obj = new ChildClass2();
+		obj.m1();
+	}
+}
